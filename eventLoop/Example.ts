9d210c827f46a7b696de6e4987da917e8814d6b1
pMichaell <michal.pawlowicz112@gmail.com>
2022-06-21 13:02:41 +0200

    added example with Promise resolving before the end of function

--- eventLoop/Example.ts
+++ eventLoop/Example.ts
@@ -1,9 +1,26 @@
-setTimeout(() => {
+/*setTimeout(() => {
   setTimeout(() => console.log("This will be logged before last"), 0);
   setTimeout(() => console.log("This will be logged last"), 0);
   console.log("Second");
 }, 0);
 
-console.log("First");
+console.log("First");*/
+
+function baz() {
+  console.log("baz");
+}
+
+async function promiseResolve() {
+  setTimeout(() => console.log("this will be last"), 0);
+  console.log("foo");
+  Promise.resolve(
+    "this will be printed before last,\n" +
+      "promises that resolv before function ends will be executed\n" +
+      "right after function ends"
+  ).then((description) => console.log(description));
+  baz();
+}
+
+promiseResolve();
 
 //TODO add microtasks
